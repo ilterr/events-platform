729d43025028d2209658f5541709f0468f7b21fe
Ilter Ahmet <ilterahmet93@gmail.com>
2025-03-21 14:13:26 +0000

    Update dashboard with minor code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import Home from "./pages/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
- return (
+  return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
- )
+  );
 }
 
 export default App;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { UserAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [signOutError, setSignOutError] = useState("");
 
-export default Dashboard
+  const { session, signOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async (e) => {
+    e.preventDefault();
+    try {
+      await signOut();
+      navigate("/");
+    } catch (error) {
+      setSignOutError(error.message);
+    }
+  };
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h2>Welcome {session?.user?.email}</h2>
+      <div>
+        <button
+          onClick={handleSignOut}
+          className="hover:cursor-pointer border inline-block px-4 py-3 mt-4"
+        >
+          Sign Out
+        </button>
+      </div>
+      {signOutError && (
+        <p className="text-red-500 text-center mt-2">{signOutError}</p>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
